e0f425d88d5499d98ae3ee3c7e91ae1a642b2de0
wylliamunlimited <wylliam@sigmasquared.io>
2026-06-08 16:41:05 -0400

    Implemented bottom border line render function

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,20 @@ fn stop_border(matrix: &Board) -> String {
 
 fn bottom_border(matrix: &Board) -> String {
     let mut output = String::new();
+
+    output.push('╚');
+    for i in 0..matrix.size {
+        output.push_str("═══");
+
+        if i == matrix.size - 1 {
+            output.push('╝');
+        } else if (i + 1) % matrix.box_size == 0 {
+            output.push('╩');
+        } else {
+            output.push('╧');
+        }
+    }
+    output.push('\n');
 }
 
 fn thick_middle_border(matrix: &Board) -> String {
