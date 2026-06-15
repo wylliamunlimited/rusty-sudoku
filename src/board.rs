126dd952cd21be8b65d89bd5ebe497ad09a4b19c
Wylliam <wylliam.cheng@gmail.com>
2026-06-14 23:12:18 -0400

    Added border style impl for enum value lookup

--- src/board.rs
+++ src/board.rs
@@ -3,3 +3,45 @@ pub struct Board {
     pub(crate) box_size: usize,
     pub(crate) cells: Vec<Vec<i32>>,
 }
+
+pub struct BorderStyle {
+    pub(crate) left: char,
+    pub(crate) fill: &'static str,
+    pub(crate) cell: char,
+    pub(crate) box_junction: char,
+    pub(crate) right: char,
+}
+
+impl BorderStyle {
+    pub(crate) const TOP: BorderStyle = BorderStyle {
+        left: '╔',
+        fill: "═══",
+        cell: '╤',
+        box_junction: '╦',
+        right: '╗',
+    };
+
+    pub(crate) const BOTTOM: BorderStyle = BorderStyle {
+        left: '╚',
+        fill: "═══",
+        cell: '╧',
+        box_junction: '╩',
+        right: '╝',
+    };
+
+    pub(crate) const THICK: BorderStyle = BorderStyle {
+        left: '╠',
+        fill: "═══",
+        cell: '╪',
+        box_junction: '╬',
+        right: '╣',
+    };
+
+    pub(crate) const THIN: BorderStyle = BorderStyle {
+        left: '╟',
+        fill: "───",
+        cell: '┼',
+        box_junction: '╫',
+        right: '╢',
+    };
+}
