375c036e9e30622e64877c1e06efdd616dc535cc
wylliamunlimited <wylliam@sigmasquared.io>
2026-06-17 16:44:30 -0400

    Adding final board render on main.rs

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod board;
 use board::Board;
 
 fn main() {
+
+    let mut board: Board = Board::new(9, 3);
     
     loop {
 
         print!("\x1B[2J\x1B[H");
         print!("{board}");
 
+        break;
     }
     
 }
