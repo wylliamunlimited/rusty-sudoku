057190adca7df943af9fefdb194feb8629b1d2f0
Wylliam <wylliam.cheng@gmail.com>
2026-07-11 23:15:50 -0400

    Added functions to translate pressed key code to game ops - isolated into I/O reading vs parsing

--- src/tui.rs
+++ src/tui.rs
@@ -16,7 +16,44 @@ impl TerminalGuard {
 
         Ok(TerminalGuard)
     }
-    
+
+    fn key_to_action(&self, key: KeyCode) -> Option<Action> {
+        // Translation Function
+
+        match key {
+            // Shift
+            KeyCode::Left => Some(Action::Move(Direction::Left)),
+            KeyCode::Right => Some(Action::Move(Direction::Right)),
+            KeyCode::Up => Some(Action::Move(Direction::Up)),
+            KeyCode::Down => Some(Action::Move(Direction::Down)),
+            
+            // Exit
+            KeyCode::Char('q') => Some(Action::Quit),
+
+            // Ops
+            KeyCode::Backspace => Some(Action::ClearCell),
+            KeyCode::Char(c @ '1'..='9') => {
+                let digit = c.to_digit(10).unwrap() as i32;
+                Some(Action::SetDigit(digit))
+            },
+            _ => None
+        }
+
+    }
+
+    fn handle_op(&self) -> io::Result<Option<Action>> {
+
+        let action = match read()? {
+            Event::Key(event) if event.kind == KeyEventKind::Press => {
+                self.key_to_action(event.code)
+            },
+            _ => None
+        };
+
+        Ok(action)
+
+    }
+
 }
 
 impl Drop for TerminalGuard {
