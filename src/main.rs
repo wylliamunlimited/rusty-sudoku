789b65b17a1503bc0051444cc0d7c9676e762db4
Wylliam <wylliam.cheng@gmail.com>
2026-07-11 23:35:53 -0400

    Wired up the TUI + App (game) impl - isolating domain logics

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,36 @@
 mod board;
 mod app;
+mod tui;
 
 use board::Board;
-use app::{App, Direction};
+use app::App;
 
-use crossterm::event::{Event, KeyCode, KeyEventKind, poll, read};
-use crossterm::{execute, terminal, terminal::{EnterAlternateScreen, LeaveAlternateScreen}};
+use tui::TerminalGuard;
+use crossterm::event::poll;
 
 use std::time::Duration;
-use std::io::Write;
 use std::io;
 
 
 fn main() -> io::Result<()> {
     let mut app: App = App::new(Board::new(9, 3));
-    
-    execute!(io::stdout(), EnterAlternateScreen)?;
-    terminal::enable_raw_mode()?;
+    let guard: TerminalGuard = TerminalGuard::new()?;
+
     loop {
 
         app.tick();
-
-        // Redraw the board in place.
-        print!("\x1B[2J\x1B[H");
-        print!("{}", app.view().replace('\n', "\r\n"));
-        print!("\nEnter 'q' to exit this window...");
-        io::stdout().flush()?;
+        guard.draw(&app)?;
 
         if poll(Duration::from_millis(500))? {
-            match read()? {
-                Event::Key(event) if event.kind == KeyEventKind::Press => {
-                    match event.code {
-                        KeyCode::Left => app.shift_cursor(Direction::Left),
-                        KeyCode::Right => app.shift_cursor(Direction::Right),
-                        KeyCode::Down => app.shift_cursor(Direction::Down),
-                        KeyCode::Up => app.shift_cursor(Direction::Up),
-                        KeyCode::Char(c @ '1'..='9') => {
-                            let digit = c.to_digit(10).unwrap() as i32;
-                            app.set_current_cell(digit);
-                        },
-                        KeyCode::Backspace => app.clear_current_cell(),
-                        KeyCode::Char('q') => break,
-                        _ => {}
-                    }
+            if let Some(action) = guard.handle_op()? {
+                if !app.handle_action(action) {
+                    break
                 }
-                _ => {}
             }
         }
     }
-    terminal::disable_raw_mode()?;
-    execute!(io::stdout(), LeaveAlternateScreen)?;
+
+    drop(guard);
 
     Ok(())
 }
