88c9620f2c55676480c7e49cf2fa624bfd8bf8f2
wylliamunlimited <wylliam@sigmasquared.io>
2026-07-31 15:58:14 -0400

    Wired up the new board initialization method to main - now live

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use rusty_sudoku::app::App;
 use rusty_sudoku::board::Board;
+use rusty_sudoku::puzzle::Puzzle;
 
 use crossterm::event::poll;
 use rusty_sudoku::tui::TerminalGuard;
@@ -8,7 +9,8 @@ use std::io;
 use std::time::Duration;
 
 fn main() -> io::Result<()> {
-    let mut app: App = App::new(Board::new(9, 3));
+    let puzzle: Puzzle = Puzzle::generate(9, 3);
+    let mut app: App = App::new(Board::from_puzzle(puzzle));
     let guard: TerminalGuard = TerminalGuard::new()?;
 
     loop {
